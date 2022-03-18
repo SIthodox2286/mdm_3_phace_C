045d29204ee442533458af033a00ff25ab928852
nj19257 <60780410+nj19257@users.noreply.github.com>
2022-03-18 13:28:17 +0000

    Update test.py

--- test.py
+++ test.py
@@ -4,30 +4,38 @@ import os
 
 def load_data(path,data_extract,model):
     df =pd.read_csv(path)
-    print(df)
-    print( df['Local Authority code'][1] )
+    #print(df)
+    #print( df['Local Authority name'][1] )
     model.insert(2, column=data_extract, value= None)
     for n in range(0, 346):
         loop(model , df, data_extract ,n)
+    #print(model)
+
+    return model
 
 
-    print(model)
 
 def loop(model , df, data_extract ,n):
     for i in range(0,429):
-        if model.values[n][0] == df['Local Authority code'][i]:
+        if model.values[n][0] == df['Local Authority code'][i] or str(model.values[n][1]) in str(df['Local Authority name'][i] ):
 
             model.at[n,data_extract] = df[data_extract][i]
 
             break
 
-    return n
-    print(model)
+
+
 
 
 
 
 model = pd.read_csv('test.csv')
 
-print(model.values[0][0])
+print(model.values[0][1])
 load_data('Number_of_Schools_UK.csv','Violence with injury',model)
+#print(model)
+
+#result section
+
+
+model.to_csv("result.csv")
